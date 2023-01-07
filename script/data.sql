8f0a3cce3349c5ac2721a495af3d3eef692bf806
Jacques <mamiharilalajacques@gmail.com>
2023-01-07 21:09:01 +0300

    [script] data menu

--- script/data.sql
+++ script/data.sql
@@ -1,3 +1,33 @@
-INSERT INTO menu
+INSERT INTO  menu
 (id, libelle, icone, src, rang, profondeur, mere)
-VALUES('MENU'||(select nextval('seq_menu'))  , 'Validation et paiement', 'micon bi bi-table', 'container.jsp?content=boutique/boutique_commande_validation.jsp', 4, 3, 'MENU4');
+VALUES('MENU'||(select nextval('seq_menu')), 'Liste', 'micon bi bi-table', 'container.jsp?content=promotion/promotion_read.jsp', 2, 3, 'MENU3');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Créer', 'icon-copy dw dw-add', 'container.jsp?content=promotion/promotion_create.jsp', 1, 3, 'MENU3');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Créer', 'icon-copy dw dw-calendar-3', 'container.jsp?content=boutique/boutique_create.jsp', 1, 3, 'MENU4');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Liste', 'micon bi bi-table', 'container.jsp?content=boutique/boutique_read.jsp', 2, 3, 'MENU4');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Liste', 'micon bi bi-table', 'container.jsp?content=produit/produit_read.jsp', 2, 3, 'MENU5');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Créer', 'icon-copy dw dw-add', 'container.jsp?content=produit/produit_create.jsp', 1, 3, 'MENU5');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Liste des commandes', 'micon bi bi-table', 'container.jsp?content=boutique/boutique_commande_read.jsp', 3, 3, 'MENU4');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Racine', 'icon-copy dw dw-home', '', 1, 1, '');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Produit', 'micon bi bi-archive', '', 1, 2, 'MENU1');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Boutique', 'micon bi bi-house', '', 2, 2, 'MENU1');
+INSERT INTO  menu
+(id, libelle, icone, src, rang, profondeur, mere)
+VALUES('MENU'||(select nextval('seq_menu')), 'Promotion', 'micon bi bi-calendar4-week', '', 3, 2, 'MENU1');
