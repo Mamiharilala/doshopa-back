7c1d0715658a8a60debea4279cae0668e290ebea
Jacques <mamiharilalajacques@gmail.com>
2023-01-15 08:22:01 +0300

    data

--- script/2023-01-13_Alter_Utilisateur.sql
+++ script/2023-01-13_Alter_Utilisateur.sql
@@ -1,1 +1,2 @@
 alter table utilisateur add column boutique_id character varying(100);
+alter table utilisateur add column has_valid_mail boolean;
--- script/data.sql
+++ script/data.sql
@@ -48,3 +48,30 @@ VALUES('MENU'||(select nextval('seq_menu')), 'Accueil', 'micon dw dw-home', '', 
 INSERT INTO  menu
 (id, libelle, icone, src, rang, profondeur, mere)
 VALUES('MENU'||(select nextval('seq_menu')), 'Tableau de bord', 'micon dw dw-home', 'container.jsp?content=boutique/boutique_dashboard.jsp', 1, 3, 'MENU1');
+
+-- JAn 15 2023
+INSERT INTO utilisateur
+(id, nom, prenom, mail, telephone, mot_passe, adresse, etat, sexe, role_id, login, has_valid_mail, boutique_id)
+VALUES('UTLS'||(select nextval('seq_utilisateur'), 'Sylvana', 'Precilia', 'sylvanaprecilia.l17@gmail.com', '0324370684', '77de68daecd823babbb58edb1c8e14d7106e83bb', 'Tsiadana ', 11, 'f', 'admin', 'sylvana', true, 'BO4676');
+INSERT INTO boutique
+(id, denomination, siege, categorie_id, logo, mail, telephone, remarque, nif, stat, etat, type_id, "views", apropos, information, note)
+VALUES('BO'||(select nexval('seq_boutique')), 'Pannini food', 'Ivato', 'BC003', '', 'sylvanaprecilia.l17@gmail.com', '0329403490', 'Livraison ÃÂ  Tana seulement', '243645', '353537', 1, 'BT002', 0, NULL, NULL, NULL);
+
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC'||(select nexval('seq_boutique_categorie'), 'Matériel informatique', 'Matériel informatique');
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC004', 'Habillement', 'Habillement');
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC005', 'Produit cosmétique', 'Produit cosmétique');
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC006', 'Piece auto_moto', 'Piece auto_moto');
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC003', 'Restaurant', 'Restaurant');
+INSERT INTO  boutique_categorie
+(id, code, description)
+VALUES('BC001', 'Accessoire de mode', 'Accessoire de mode');
