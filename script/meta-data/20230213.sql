68545cd107310e485ca5e2bb64604eddd032917d
Jacques <mamiharilalajacques@gmail.com>
2023-02-14 21:36:06 +0300

    Update 20230213.sql

--- script/meta-data/20230213.sql
+++ script/meta-data/20230213.sql
@@ -3,3 +3,5 @@ create table sexe(
 	code varchar(100) NOT NULL,
 	description varchar(100) NOT NULL
 );
+create or replace view utilisateur_token as
+select utilisateur.*,token from utilisateur join token on token.utilisateur_id = utilisateur.id 
