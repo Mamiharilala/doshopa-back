ba20dd9c2ed28b28d72706f20b98de03c11ed9c8
Jacques <mamiharilalajacques@gmail.com>
2023-02-17 01:01:15 +0300

    Update 20230215.sql

--- script/meta-data/20230215.sql
+++ script/meta-data/20230215.sql
@@ -28,3 +28,14 @@ AS SELECT
      JOIN article_promotion promotion ON article.id::text = promotion.article_id::text 
      join etat on etat.id = promotion.etat ;
 create or replace view promotion_valider as select * from v_article_promotion where etat >=11
+
+
+create table notification(
+	id varchar(100) NOT NULL,
+	message varchar(250) NOT NULL,
+	utilisateur_id varchar(100),
+	role_id varchar(100),
+	date_insert DATE not null default CURRENT_DATE,
+	has_viewed boolean not null default false
+);
+CREATE SEQUENCE seq_notification INCREMENT BY 1 MINVALUE 1 START 1 CACHE 1 NO CYCLE;
