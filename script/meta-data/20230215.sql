b971204eee76f8323541eadfb248d31ccbb30c47
Jacques <mamiharilalajacques@gmail.com>
2023-02-17 21:46:20 +0300

    Update 20230215.sql

--- script/meta-data/20230215.sql
+++ script/meta-data/20230215.sql
@@ -4,9 +4,8 @@ ADD COLUMN date_expired DATE not null default CURRENT_DATE;
 
 drop view v_article_promotion;
 CREATE OR REPLACE VIEW v_article_promotion
-AS SELECT 
-	promotion.id,
-	article.id as article_id ,
+AS SELECT promotion.id,
+    article.id AS article_id,
     article.designation,
     article.prix,
     article.reference,
@@ -15,18 +14,20 @@ AS SELECT
     article.categorie_id,
     article.boutique_id,
     article.devise_id,
-	promotion.prix_avant,
+    promotion.prix_avant,
     promotion.prix_actuel,
     promotion.date_debut,
     promotion.date_fin,
     promotion.quantite,
     promotion.etat,
-	etat.description as etatlib,
+    etat.description AS etatlib,
     article.devise,
-    promotion."views" 
-   FROM article_and_devise article 
-     JOIN article_promotion promotion ON article.id::text = promotion.article_id::text 
-     join etat on etat.id = promotion.etat ;
+    promotion.views,
+    ac.code as categorielib
+   FROM article_and_devise article
+     JOIN article_promotion promotion ON article.id::text = promotion.article_id::text
+     join article_categorie ac on ac.id = article.categorie_id 
+     JOIN etat ON etat.id = promotion.etat;
 create or replace view promotion_valider as select * from v_article_promotion where etat >=11
 
 
@@ -39,3 +40,6 @@ create table notification(
 	has_viewed boolean not null default false
 );
 CREATE SEQUENCE seq_notification INCREMENT BY 1 MINVALUE 1 START 1 CACHE 1 NO CYCLE;
+
+create or replace view etat_categorie as
+SELECT * FROM ETAT e where id in ('11','0','1');
