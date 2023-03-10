42936d52d1ded2c72c9efa5739f4a8f58da781f5
Jacques <mamiharilalajacques@gmail.com>
2023-03-10 23:29:50 +0300

    Update 20230226.sql

--- script/meta-data/20230226.sql
+++ script/meta-data/20230226.sql
@@ -7,3 +7,11 @@ CREATE TABLE visit (
 	CONSTRAINT seq_visit_pkey PRIMARY KEY (id)
 );
 CREATE SEQUENCE seq_visit INCREMENT BY 1 MINVALUE 1 START 1 CACHE 1 NO CYCLE;
+CREATE TABLE localisation_livraison (
+	id varchar(50) NOT NULL,
+ 	localisation varchar(100) NOT NULL,
+	observation varchar(100),
+	est_gratuit boolean NOT NULL default false,
+	CONSTRAINT seq_loc_livraison_pkey PRIMARY KEY (id)
+);
+CREATE SEQUENCE seq_localisation_livraison INCREMENT BY 1 MINVALUE 1 START 1 CACHE 1 NO CYCLE;
